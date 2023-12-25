2a13ee06b9786a4fd526d269bf78b0c0f5ef049a
Saket Narayan <saket@saket.me>
2023-12-25 15:54:34 -0500

    Update example usages

--- library/src/main/kotlin/me/saket/filesize/FileSize.kt
+++ library/src/main/kotlin/me/saket/filesize/FileSize.kt
@@ -10,11 +10,12 @@ import kotlin.math.roundToLong
  *
  * ```
  * val cacheSize = 512.megabytes
- * println(cacheSize)  // "512 MB"
- * println(cacheSize.bytes) // "512000000"
- * println(cacheSize < 1.gigabytes) // "true"
- * println(cacheSize * 2)  // "1.02 GB"
- *```
+ * println(cacheSize.toString())          // "512 MB"
+ * println(cacheSize.inWholeBytes)        // "512000000"
+ * println(cacheSize + 88_000.kilobytes)  // "600 MB"
+ * println(cacheSize * 2)                 // "1.02 GB"
+ * println(cacheSize < 1.gigabytes)       // "true"
+ * ```
  */
 @Poko
 class FileSize(private val bytes: Long) : Comparable<FileSize> {
