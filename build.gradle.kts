c926e7aed3eb8881b0f19fc3539e64bd1fdf8427
Goooler <wangzongler@gmail.com>
2023-12-29 10:42:25 +0800

    Revert Spotless changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,29 +1,11 @@
-import com.diffplug.gradle.spotless.SpotlessExtension
-
 plugins {
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.mavenPublish) apply false
   alias(libs.plugins.dokka) apply false
   alias(libs.plugins.poko) apply false
-  alias(libs.plugins.spotless) apply false
 }
 
 allprojects {
-  plugins.apply(rootProject.libs.plugins.spotless.get().pluginId)
-  extensions.configure<SpotlessExtension> {
-    kotlin {
-      target("src/**/*.kt")
-      ktlint(rootProject.libs.ktlint.get().version).editorConfigOverride(
-        mapOf(
-          "ktlint_standard_filename" to "disabled",
-        ),
-      )
-    }
-    kotlinGradle {
-      ktlint(rootProject.libs.ktlint.get().version)
-    }
-  }
-
   // Configure Java to use our chosen language level. Kotlin will automatically pick this up.
   // See https://kotlinlang.org/docs/gradle-configure-project.html#gradle-java-toolchains-support
   plugins.withType<JavaBasePlugin>().configureEach {
