37b305ea3f0a368f4d5cec3f1318327244d25647
Saket Narayan <saket@saket.me>
2024-09-14 16:45:32 -0400

    Set JVM target to 11

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,17 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+
 plugins {
   alias(libs.plugins.kotlin.multiplatform) apply false
   alias(libs.plugins.mavenPublish) apply false
   alias(libs.plugins.dokka) apply false
   alias(libs.plugins.metalava) apply false
+}
+
+allprojects {
+  tasks.withType<KotlinJvmCompile>().configureEach {
+    compilerOptions {
+      jvmTarget.set(JvmTarget.JVM_11)
+    }
+  }
 }
