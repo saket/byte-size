d8a17015729cc52662f1a28e269fb4d933ceb532
Jake Wharton <jw@jakewharton.com>
2026-03-16 00:24:50 -0400

    Use AssertK helper to check toString

--- library/src/commonTest/kotlin/me/saket/bytesize/asserts.kt
+++ library/src/commonTest/kotlin/me/saket/bytesize/asserts.kt
@@ -2,9 +2,9 @@ package me.saket.bytesize
 
 import assertk.Assert
 import assertk.assertions.isEqualTo
+import assertk.assertions.toStringFun
 
 internal fun Assert<ByteSize>.isApproximatelyEqualTo(other: ByteSize) {
   // todo: find a better way that does not involve comparing their formatted strings
-  this.transform { it.toString() }
-    .isEqualTo(other.toString())
+  toStringFun().isEqualTo(other.toString())
 }
